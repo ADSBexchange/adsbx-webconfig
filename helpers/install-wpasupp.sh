47d97c188d63bbc4a53b9c1369f70c764743e2d7
dan-adsbx <129466232+dan-adsbx@users.noreply.github.com>
2024-04-26 17:44:59 -0700

    Update install-wpasupp.sh
    Updates for NetworkManager based configuration

--- helpers/install-wpasupp.sh
+++ helpers/install-wpasupp.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-cp /tmp/webconfig/wpa_supplicant.conf /boot/wpa_supplicant.conf
-rm -f /tmp/webconfig/wpa_supplicant.conf
+cp /tmp/webconfig/adsbx-uiconfig.nmconnection /etc/NetworkManager/system-connections/adsbx-uiconfig.nmconnection
+chmod 600 /etc/NetworkManager/system-connections/*
+rm -f /tmp/webconfig/adsbx-uiconfig.nmconnection
+raspi-config nonint do_wifi_country $(cat /tmp/webconfig/wificountry)
+nmcli 
 sleep 5
 reboot now
 
+  
